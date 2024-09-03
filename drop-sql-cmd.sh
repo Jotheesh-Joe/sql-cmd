cc7a080b55b878ec8dc207c0d5e4af3b7351a66b
Jotheesh-Joe <58363147+Jotheesh-Joe@users.noreply.github.com>
2024-09-03 15:05:28 +0100

    Create drop-sql-cmd.sh

---
+++ drop-sql-cmd.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$server" ] || [ -z "$databases" ]; then
+    echo "One or more required environment variables are not set. Please ensure 'server' and 'databases' are set."
+    exit 1
+fi
+
+# Define the list of databases
+IFS=',' read -r -a database_array <<< "$databases"
+
+# Extracting Access Token
+az login --federated-token "$(cat $AZURE_FEDERATED_TOKEN_FILE)" --service-principal -u $AZURE_CLIENT_ID -t $AZURE_TENANT_ID
+
+TOKEN="/tmp/token"  # Replace this with your actual token or ensure it's set in the environment
+
+# Loop through each database and run the sqlcmd command
+for db in "${database_array[@]}"; do
+    echo "Processing database: $db"
+
+    if [[ "$db" == "order_microservice_ie_mo0" ]]; then
+        echo "Running additional command for database: $db"
+		sqlcmd -S "$server" -d "$db" -G -P "$TOKEN" -Q "DROP TABLE dbo.voucher;"
+		exit_status=$?  # Capture the exit status
+		if [ $exit_status -ne 0 ]; then
+            echo "Error occurred while processing table dbo.voucher in database: $db"
+        else
+            echo "Successfully processed dbo.voucher table in database: $db"
+        fi
+    fi
+
+    # Run the sqlcmd command for the first table
+    sqlcmd -S "$server" -d "$db" -G -P "$TOKEN" -Q "DROP TABLE dbo.socrates_store_database;"
+    exit_status1=$?  # Capture the exit status
+    if [ $exit_status1 -ne 0 ]; then
+        echo "Error occurred while processing table dbo.socrates_database_server in database: $db"
+    else
+        echo "Successfully processed dbo.socrates_store_database table in database: $db"
+    fi
+
+    # Run the sqlcmd command for the second table
+    sqlcmd -S "$server" -d "$db" -G -P "$TOKEN" -Q "DROP TABLE dbo.socrates_database_server;"
+    exit_status2=$?  # Capture the exit status
+    if [ $exit_status2 -ne 0 ]; then
+        echo "Error occurred while processing table dbo.socrates_database_server in database: $db"
+    else
+        echo "Successfully processed dbo.socrates_database_server table in database: $db"
+    fi
+done
